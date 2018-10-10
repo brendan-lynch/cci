3e77fa0b917064dffbfe3e1c815a55eec323fa29
Brendan Lynch <blynch87@gmail.com>
2018-10-09 23:54:48 -0700

    Chapter 1, 1-4
    Give a string, write a function to check if it is a permutation of a
    palindrome.

---
+++ chpt-1/1-4/src/Solution.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String [] args)
+    {
+        System.out.println(pp("tact Coa") + " == true");
+        System.out.println(pp(" ") + " == true");
+        System.out.println(pp("moo oN") + " == false");
+    }
+
+    private static boolean pp(String value)
+    {
+        // Why does string builder not support regex or to lower?
+        // A char array would be the best place to support this.
+        StringBuilder input = new StringBuilder(value.toLowerCase().replaceAll("\\s", ""));
+
+        if(input.length() < 2)
+        {
+            return true;
+        }
+
+        Set<Character> exists = new HashSet<>();
+        char c;
+        for(int i = 0; i < input.length(); i++)
+        {
+            c = input.charAt(i);
+            if(exists.contains(c))
+            {
+                exists.remove(c);
+            }
+            else
+            {
+                exists.add(c);
+            }
+        }
+
+        return (exists.size() == 0 && input.length() % 2 == 0) ||
+                (exists.size() == 1 && input.length() % 2 == 1);
+    }
+}
